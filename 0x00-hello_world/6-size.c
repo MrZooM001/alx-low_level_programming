909b22128f6caa127a9ae10b3595d1b68dad23db
Hazem Magdy <hhazem0010@gmail.com>
2023-06-15 14:11:16 -0700

    Update size of C program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ char c;
 int i;
 float f;
 
-printf("Size of a char: %c byte(s)\n", (unsigned char)sizeof(c));
+printf("Size of a char: %d byte(s)\n", (unsigned char)sizeof(c));
 printf("Size of an int is: %d byte(s)\n", (unsigned int) sizeof(i));
-printf("Size of a long int is: %ld byte(s)\n", (unsigned long int)sizeof(i));
-printf("Size of a long long int: %lld byte(s)\n", (unsigned long long int)sizeof(i));
-printf("Size of a float is: %d byte(s)\n",(unsigned int)sizeof(f));
+printf("Size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }

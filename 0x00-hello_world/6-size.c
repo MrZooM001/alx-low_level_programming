dfb990fa4656eb28a5154074eb0f286fa155f4e8
Hazem Magdy <hhazem0010@gmail.com>
2023-06-15 14:20:45 -0700

    Update size of C program with int conversion

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,14 @@
 /**
 * main - Entry point
 * Description: Print sizes of some of data types.
-* Return: 0
+* Return: 0 (success)
 */
 int main(void)
 {
-char c;
-int i;
-float f;
-
-printf("Size of a char: %d byte(s)\n", (unsigned char)sizeof(c));
-printf("Size of an int is: %d byte(s)\n", (unsigned int) sizeof(i));
-printf("Size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+printf("Size of an int is: %d byte(s)\n", (i)sizeof(int));
+printf("Size of a long int is: %d byte(s)\n", (int)sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+printf("Size of a float is: %d byte(s)\n", (int)sizeof(float));
 return (0);
 }

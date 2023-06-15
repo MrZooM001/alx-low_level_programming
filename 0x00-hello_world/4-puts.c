643d51eadbb35d8c9b437eb8f9a5139d35b9e698
Hazem Magdy <hhazem0010@gmail.com>
2023-06-15 12:50:48 -0700

    Add C program that prints exact message, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - The entry point of the program
+* Description: Print exact message, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

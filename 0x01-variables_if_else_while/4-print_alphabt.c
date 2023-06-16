d68884db316f7ef687f80a56f50abfa69c440051
Hazem Magdy <hhazem0010@gmail.com>
2023-06-16 13:39:54 -0700

    Add C program that print alphabet except some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Start point of the program
+* Description: Print alphabet in lowercase then uppercase
+* Return: 1 if success
+*/
+int main(void)
+{
+char c;
+
+c = 'a';
+while (c <= 'z')
+{
+if (c == 'e')
+{
+}
+else if (c == 'q')
+{
+}
+else
+{
+putchar(c);
+}
+c++;
+}
+putchar('\n');
+
+return (0);
+}

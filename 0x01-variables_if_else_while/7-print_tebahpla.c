170121456203cba004921dfcb5f7a605c152f455
Hazem Magdy <hhazem0010@gmail.com>
2023-06-16 14:02:04 -0700

    Add C Program that print & reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Start point of the program
+* Description: Print alphabet in lowercase
+* Return: 1 if success
+*/
+int main(void)
+{
+char c;
+
+c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+
+return (0);
+}

1eed4a1cffcc0f2366a68be65d13c3bc0d26bedf
Hazem Magdy <hhazem0010@gmail.com>
2023-06-16 14:14:35 -0700

    Add C program that print hexa numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Start point of the program
+* Description: Print hexa numbers
+* Return: 1 if success
+*/
+int main(void)
+{
+for (int n = 48; n <= 122; n++)
+{
+if (n >= 48 && n <= 57)
+{
+putchar(n);
+}
+else if (n >= 97 && n <= 122)
+{
+putchar(n);
+}
+else
+{
+}
+}
+putchar('\n');
+
+return (0);
+}

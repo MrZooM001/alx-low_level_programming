2848e6d5eca21c81c880019699ca1df6048a7b76
Hazem Magdy <hhazem0010@gmail.com>
2023-06-16 13:50:24 -0700

    Add C program that prints from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Start point of the program
+* Description: Print numbers from 0 to 9
+* Return: 1 if success
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+
+return (0);
+}

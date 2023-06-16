2ff8ec81e0c80c909be542ad368a7df29de22b2d
Hazem Magdy <hhazem0010@gmail.com>
2023-06-16 13:59:49 -0700

    Add C program that putchar 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Start point of the program
+* Description: Print numbers from 0 to 9
+* Return: 1 if success
+*/
+int main(void)
+{
+int n;
+
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n');
+
+return (0);
+}

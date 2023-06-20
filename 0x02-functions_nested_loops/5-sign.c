b1e4ff3c174f4ea9ad1f89b5736dd7787e10c1ec
Hazem Magdy <hhazem0010@gmail.com>
2023-06-20 13:49:18 -0700

    Add C program that checks a given number is positive, negative or zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if a given is positive or negative
+ * @n: integer number to checked
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+}

2d404409ddf9ba8644b71d502e489908a4c48072
Hazem Magdy <hhazem0010@gmail.com>
2023-06-20 08:18:03 -0700

    Add new directory for function_nested_loops project, then add C program to print _putchar using function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to be printed
+ *
+ * Return: 1 if success
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+#include <unistd.h>
+
+write(1, &c, 1);
+
+return (unsigned char)c;
+}
+
+/**
+ * main - Start point of the program
+ * Description: Print some message using custom function
+ *
+ * Return: 1 if success
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar(32);
+
+return (0);
+}

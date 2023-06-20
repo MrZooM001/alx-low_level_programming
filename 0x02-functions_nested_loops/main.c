713fe2f6e2281cbd49bdd9b2202c198c3492eb0f
Hazem Magdy <hhazem0010@gmail.com>
2023-06-20 08:44:38 -0700

    Add README.md, main.c, main.h files and update 0-putchar.c file

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Start point of the program
+ * Description: Print some message using custom function
+ *
+ * Return: 1 if success
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar('\n');
+
+return (0);
+}

8178764b585b8e52f9110c6d5b5b3bde11aa7810
Hazem Magdy <hhazem0010@gmail.com>
2023-06-20 10:07:04 -0700

    Add C program that prints alphabet in lowercase using nested functions and defined header

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Start point of the program
+ * Description: Print a message using nested functions
+ *
+ * Return: 1 if success
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+
+print_alphabet();
+
+return (0);
+}

cf604d165ec23a85dbe35e48c64b0ca0764f39a0
Hazem Magdy <hhazem0010@gmail.com>
2023-06-20 18:59:51 -0700

    Update C program that prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,30 @@ int i;
 
 if (n < 98)
 {
-for (i = n; i <= 89; i++)
+for (i = n; i <= 98; i++)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else
 {
 printf("%d, ", i);
+}
 }
 }
 else if (n > 98)
 {
-for (i = n; i >= 89; i--)
+for (i = n; i >= 98; i--)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else
 {
 printf("%d, ", i);
+}
 }
 }
 putchar('\n');

48d8fbc2ed9bb36cf1956150df9cbe8fd19dd226
Hazem Magdy <hhazem0010@gmail.com>
2023-06-20 17:55:26 -0700

    Add C program that that adds two integers and returns the result through a function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - prints every minute of the day in 24h format
+ * @a: An integer number to be added as first operand
+ *@b: An integer number to be added as second operand
+ *
+ * Return: the result of sum a + b
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}

b537ba7efcaa4326263d6b99366cd92a62d07379
Hazem Magdy <hhazem0010@gmail.com>
2023-06-21 23:14:26 -0700

    Add C function that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if given character is digit
+ * @c: An integer number to checked
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

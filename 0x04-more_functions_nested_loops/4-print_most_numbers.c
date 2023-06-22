0089a8553ea00dbb2eb8f9879e54799a870da6ca
Hazem Magdy <hhazem0010@gmail.com>
2023-06-21 23:41:37 -0700

    Add multiple C functions files and _putchar.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 except 2 & 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+int n;
+
+n = 0;
+while (n <= 9)
+{
+if (n != 2 || n != 4)
+{
+_putchar(n + '0');
+}
+n++;
+
+}
+_putchar('\n');
+}

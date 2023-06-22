e4c39b50aae8d6af287b7b9290a5bea69a774c67
Hazem Magdy <hhazem0010@gmail.com>
2023-06-22 09:36:21 -0700

    Add C program 104-fibonacci.c from advanced tasks

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
+#define LARGEST 100000000000
 
 /**
- * main - Start point of the program
- * Description: Prints the first 98 Fibonacci numbers,
+ * main - Start point of the program 
+ * Description: Prints the first 98 Fibonacci numbers, 
  * starting with 1 and 2, followed by a new line.
  *
  * Return: 0 if success
  */
 int main(void)
 {
-unsigned long int a, b, r;
+unsigned long int a, b, c1, c2;
+unsigned long int h1, h2, h3;
 int i;
 
-a = 0;
-b = 1;
-for (i = 1; i <= 98; i++)
+a = 0, b = 1, c1 = 0, c2 = 0;
+for (i = 2; i < 98; i++)
 {
-r = a + b;
-if (i < 98)
+if (a + b > LARGEST || c2 > 0 || c1 > 0)
 {
-printf("%lu, ", r);
+h1 = (a + b) / LARGEST;
+h2 = (a + b) % LARGEST;
+h3 = a + b + h1;
+a = b, b = h3;
+a = b, b = h2;
+printf("%lu%010lu", c2, b);
 }
 else
 {
-printf("%lu\n", r);
+h2 = a + b;
+a = b, b = h2;
+printf("%lu", b);
 }
-a = b;
-b = r;
+if (i != 97)
+{
+printf(", ");
 }
+}
+printf("\n");
 
 return (0);
 }

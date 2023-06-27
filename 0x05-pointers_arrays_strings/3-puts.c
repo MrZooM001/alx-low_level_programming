5cf67f47fb8a06b9e8311bfad8c8ee2bbceb5db5
Hazem Magdy <hhazem0010@gmail.com>
2023-06-26 19:32:13 -0700

    Add C function 3-puts.c that prints a string followed by new line to standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @s: Pointer to a string.
+ * Description: A function that prints a string,
+ * followed by a new line, to standard output.
+ *
+ * Return: void.
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}

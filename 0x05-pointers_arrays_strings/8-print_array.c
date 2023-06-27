5d76845190db172aebfb5630cf4695a66072400e
Hazem Magdy <hhazem0010@gmail.com>
2023-06-27 16:23:01 -0700

    Update C function 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,11 @@
 void print_array(int *a, int n)
 {
 int i;
-if (n > 0)
+if (n <= 0)
+{
+n = 0;
+}
+else if (n > 0)
 {
 for (i = 0; i < n; i++)
 {
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to in source, to the buffer in destination.
+ * @dest: Pointer to a string.
+ * @src: Pointer to a string.
+ * Description: A function that copies the string pointed to by src, 
+ * to the buffer pointed to by dest, including the terminating null byte (\0),
+ * and returns the pointer of destination.
+ *
+ * Return: void.
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+
+_putchar('\n');
+}

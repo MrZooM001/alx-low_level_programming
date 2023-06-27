5aecee878f96db80172460c6833666ed3f47a95a
Hazem Magdy <hhazem0010@gmail.com>
2023-06-26 21:37:39 -0700

    Update C function 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,18 +10,18 @@
  */
 void print_rev(char *s)
 {
-int length;
+int i, length;
 
+length = 0;
 while (*s != '\0')
 {
 length++;
 s++;
 }
 s--;
-while (length > 0)
+for (i = length; i > 0; i--)
 {
 _putchar(*s);
-length--;
 s--;
 }
 _putchar('\n');

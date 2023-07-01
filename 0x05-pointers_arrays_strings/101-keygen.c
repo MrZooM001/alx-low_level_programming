3f86c8b3de487eacf77d0cba5e7975f4eb916d52
Hazem Magdy <hhazem0010@gmail.com>
2023-07-01 03:58:49 -0700

    Update C function that generates random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PWD_LENGTH 16
-
 /**
  * main - Start point
  *
@@ -11,16 +9,17 @@
  */
 int main(void)
 {
-char password[PWD_LENGTH];
+int pwd[100];
 int i, sum, n;
 
 sum = 0;
 srand(time(NULL));
-for (i = 0; i < PWD_LENGTH; i++) {
-password[i] = rand() % 78 + '0';
-putchar(password[i]);
-sum += (password[i] - '0');
-if ((2772 - sum) - '0' < 78)
+for (i = 0; i < 100; i++)
+{
+pwd[i] = rand() % 78;
+sum += (pwd[i] + '0');
+putchar(pwd[i] + '0');
+if (((2772 - sum)-'0') < 78)
 {
 n = 2772 - sum - '0';
 sum += n;
@@ -28,7 +27,6 @@ putchar(n + '0');
 break;
 }
 }
-putchar('\n');
 
 return (0);
 }

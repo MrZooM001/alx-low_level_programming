ad528b70eaee0e6462a8b3757b327f18bf73e8b0
Hazem Magdy <hhazem0010@gmail.com>
2023-07-23 04:17:41 -0700

    C function that print numbers passed as parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers that passed as parameters.
+ * @separator: A const pointer to character.
+ * @n: An unsigned integer as number of passed parameters.
+ *
+ * Return: Void.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i, num;
+va_list args;
+
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+num = va_arg(args, unsigned int);
+printf("%d", num);
+if (separator != NULL)
+{
+if (i != (n - 1))
+{
+printf("%s", separator);
+}
+}
+}
+_putchar('\n');
+}

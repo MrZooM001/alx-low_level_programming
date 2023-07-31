767278eed4f4cb7ceb017629ec4f5d3f405bbb77
Hazem Magdy <hhazem0010@gmail.com>
2023-07-30 20:13:29 -0700

    Fix betty styling

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,20 +12,17 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node;
-list_t *current_node;
+list_t *new_node, *current_node;
 unsigned int length;
 
 if (str == NULL)
-{
 return (NULL);
-}
+
 length = 0;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
-{
 return (NULL);
-}
+
 new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
@@ -33,15 +30,12 @@ free(new_node);
 return (NULL);
 }
 while (str[length] != '\0')
-{
 length++;
-}
+
 new_node->len = length;
 new_node->next = NULL;
 if (*head == NULL)
-{
 *head = new_node;
-}
 else
 {
 current_node = *head;

da8d0d2da3a0c21bbde8a73fdaa0dfe0ca839f5e
Hazem Magdy <hhazem0010@gmail.com>
2023-08-01 16:56:48 -0700

    Fix the function's prototype

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdarg.h>
 #include "lists.h"
 
 /**
@@ -10,7 +9,7 @@
  *
  * Return: the nth node of a listint_t linked list.
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int n;
 

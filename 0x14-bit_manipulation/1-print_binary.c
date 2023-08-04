f63e9926f6b0a5e6cc86601c0490e0572f3d3c40
Hazem Magdy <hhazem0010@gmail.com>
2023-08-03 17:49:19 -0700

    Function to print the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - A function that prints
+ * prints the binary representation of a number.
+ * @b: An unsigned long integer.
+ *
+ * Return:  Void.
+ */
+void print_binary(unsigned long int n)
+{
+if(n > 1)
+{
+print_binary(n >> 1);
+}
+_putchar((n & 1) + '0');
+}

b7623bf80276b6ad83f168683055f7932de71aea
Hazem Magdy <hhazem0010@gmail.com>
2023-08-22 13:20:25 -0700

    Fix combarisong datatypes

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int bit_calc;
-int bit_size = sizeof(*n) * 8;
+unsigned int bit_size = sizeof(*n) * 8;
 
 if (index >= bit_size)
 {

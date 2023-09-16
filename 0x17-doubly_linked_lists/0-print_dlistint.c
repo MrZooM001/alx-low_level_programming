89d5ba4dcf5c188d7529791ad001ce022b325c61
Hazem Magdy <hhazem0010@gmail.com>
2023-09-16 05:01:22 -0700

    doubly linked lists task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+unsigned long i;
+
+i = 1;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+
+return (--i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Projects of **0x17. C - Doubly linked lists**

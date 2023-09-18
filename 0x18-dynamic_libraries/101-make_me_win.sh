247bb607cad8cd7a5c59f96600c4ed860ef874e1
Hazem Magdy <hhazem0010@gmail.com>
2023-09-18 13:52:10 -0700

    rename to libgiga task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MrZooM001/alx-low_level_programming/tree/main/0x18-dynamic_libraries/wingiga.so
-export LD_PRELOAD=/tmp/wingiga.so
+wget -P /tmp https://github.com/MrZooM001/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so

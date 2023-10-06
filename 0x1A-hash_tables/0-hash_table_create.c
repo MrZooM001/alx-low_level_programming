1e7bab554299a1ed4a11443c9e81d85d43671ac7
Hazem Magdy <hhazem0010@gmail.com>
2023-10-06 10:50:36 -0700

    hash tables task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+
+/**
+ * _calloc - Function that allocates memory for an array, using malloc.
+ *
+ * @nmemb: An unsigned integer as number of elements.
+ * @size:  An unsigned integer as size of each element.
+ *
+ * Return: Void.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, length;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	length = nmemb * size;
+	ptr = malloc(length);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
+
+/**
+ * hash_table_create - function that creates a hash table.
+ *
+ * @size: The size of the array
+ *
+ * Return: a pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_node_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = _calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+Tasks of ***0x1A. C - Hash tables*** Project <br />
+Scope <br />
+***C language*** <br />

8f417d397267989e6512dd218433d1b1a54c3d45
Hazem Magdy <hhazem0010@gmail.com>
2023-10-08 10:38:31 -0700

    fix 7 table size task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -39,12 +39,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *table;
 unsigned long int i;
 
-table = malloc(sizeof(hash_table_t *) * size);
+table = malloc(sizeof(hash_table_t));
 if (table == NULL)
 return (NULL);
 
 table->size = size;
-table->array = _calloc(table->size, sizeof(hash_node_t));
+table->array = malloc(size * sizeof(hash_node_t *));
 if (table->array == NULL)
 {
 free(table);
